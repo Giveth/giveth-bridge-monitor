27d34f5682bfe7604cad1a3d39bf61085376b883
Bowen Sanders <bowen@giveth.io>
2019-05-10 15:48:25 -0700

    import tab-styles.css

--- src/App.js
+++ src/App.js
@@ -2,13 +2,30 @@ import React, { Component } from 'react';
 import BridgeMonitor from './components/BridgeMonitor';
 
 import './styles/styles.css';
+import './styles/tab-styles.css';
 import 'react-table/react-table.css';
 import 'react-tabs/style/react-tabs.css';
+
+import Web3 from 'web3';
+
+import Web3Provider from 'web3-react'
+import { Connectors } from 'web3-react'
+const { InjectedConnector } = Connectors
+
+const MetaMask = new InjectedConnector({ supportedNetworks: [1, 4] })
+
+const connectors = { MetaMask }
+
 
 class App extends Component {
   render() {
     return (
-      <BridgeMonitor />
+      <Web3Provider
+        connectors={connectors}
+        libraryName={'web3.js'}
+        web3Api={Web3} >
+        <BridgeMonitor />
+      </Web3Provider>
     );
   }
 }
