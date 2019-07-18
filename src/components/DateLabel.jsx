d9c18b800a5ace0a85e252fcd6923b1fd9e76057
Frederik Bolding <frederik.bolding@gmail.com>
2019-07-18 18:51:23 +0200

    Added check for invalid dates

--- src/components/DateLabel.jsx
+++ src/components/DateLabel.jsx
@@ -11,9 +11,13 @@ class DateLabel extends React.Component {
     }
 
     render() {
-        const timezone = moment.tz.guess();
-        const date = moment.tz(this.props.date, "UTC").tz(timezone);
-        return (<span onClick={() => { this.setState(prevState => ({ showTimeLeft: !prevState.showTimeLeft })) }}>{this.state.showTimeLeft ? (date.fromNow()) : (date.format("ddd, D MMM YYYY HH:mm:ss z"))}</span>)
+        if (this.props.date) {
+            const timezone = moment.tz.guess();
+            const date = moment.tz(this.props.date, "UTC").tz(timezone);
+            return (<span onClick={() => { this.setState(prevState => ({ showTimeLeft: !prevState.showTimeLeft })) }}>{this.state.showTimeLeft ? (date.fromNow()) : (date.format("ddd, D MMM YYYY HH:mm:ss z"))}</span>)
+        } else {
+            return (<span>Invalid Date</span>);
+        }
     }
 }
 
